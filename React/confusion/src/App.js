9ae68db7f100df627e31f0a9869291c9af1c779b
Ritika Sharma <ritika.sh2612@gmail.com>
2020-03-13 00:54:25 -1000

    onClick event and state change

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/Dishes';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+  CardTitle } from 'reactstrap';
 import './App.css';
 
 export default class App extends Component{  
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  
+
+
+
   render(){ // equivalent to React.render
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href='/'>Cafe Confusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes = {this.state.dishes}/>
       </div>
     );
   }
